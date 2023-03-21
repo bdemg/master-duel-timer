96250dc1cd4d764d6798056ea49ae5f34e7e5215
Jorge Cano <jacano.sosa@gmail.com>
2023-03-21 10:59:44 -0600

    Add settings page and use those timer settings

---
+++ app/src/main/java/com/example/masterdueltimer/DuelSettings.kt
@@ -0,0 +1,21 @@
+package com.example.masterdueltimer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class DuelSettings: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(android.R.id.content, SettingsFragment())
+            .commit()
+    }
+}
+class SettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
