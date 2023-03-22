dfd469bd208997d398053e9e9331dd333acdfa9d
Jorge Cano <jacano.sosa@gmail.com>
2023-03-22 09:03:35 -0600

    Force the numeric keyboard for the timer settings

--- app/src/main/java/com/example/masterdueltimer/DuelSettings.kt
+++ app/src/main/java/com/example/masterdueltimer/DuelSettings.kt
@@ -1,8 +1,13 @@
 package com.example.masterdueltimer
 
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+
 
 class DuelSettings: AppCompatActivity(){
 
@@ -17,5 +22,33 @@ class DuelSettings: AppCompatActivity(){
 class SettingsFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        val maxTimeField = findPreference<EditTextPreference>("maxTime")
+        maxTimeField!!.setOnBindEditTextListener { editText ->
+            editText.inputType =
+                InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+            editText.transformationMethod = NumericKeyBoardTransformationMethod()
+        }
+
+        val turnEndGain = findPreference<EditTextPreference>("turnEndGain")
+        turnEndGain!!.setOnBindEditTextListener { editText ->
+            editText.inputType =
+                InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+            editText.transformationMethod = NumericKeyBoardTransformationMethod()
+        }
+
+        val turnStartGain = findPreference<EditTextPreference>("turnStartGain")
+        turnStartGain!!.setOnBindEditTextListener { editText ->
+            editText.inputType =
+                InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_VARIATION_PASSWORD
+            editText.transformationMethod = NumericKeyBoardTransformationMethod()
+        }
+    }
+}
+
+private class NumericKeyBoardTransformationMethod :
+    PasswordTransformationMethod() {
+    override fun getTransformation(source: CharSequence, view: View?): CharSequence {
+        return source
     }
 }
